f088cdcf0b7960ad1d2f5f39ce59ab4d0d07a017
Mauri2591 <mauriciorgonzalez25@gmail.com>
2026-07-29 19:15:40 -0300

    Se actualiza

---
+++ tasks/osint/__init__.py
@@ -0,0 +1,21 @@
+from .handlers import (
+    discovery_subdominios,
+    enumeracion_servicios,
+    mapeo_ips,
+    recon_cloud,
+    escaneo_repositorios,
+    analisis_dns,
+    busqueda_endpoints,
+    google_dorking
+)
+
+__all__ = [
+    'discovery_subdominios',
+    'enumeracion_servicios',
+    'mapeo_ips',
+    'recon_cloud',
+    'escaneo_repositorios',
+    'analisis_dns',
+    'busqueda_endpoints',
+    'google_dorking'
+]
