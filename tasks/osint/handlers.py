4e5206c1c8954b747da23065055730c206c0be4d
Mauri2591 <mauriciorgonzalez25@gmail.com>
2026-07-31 16:54:52 -0300

    Se actualiza

--- tasks/osint/handlers.py
+++ tasks/osint/handlers.py
@@ -7,6 +7,7 @@ from datetime import datetime
 import subprocess
 import socket
 import requests
+import os
 
 # ══════════════════════════════════════════════════════════════════
 # HELPERS GLOBALES OSINT
