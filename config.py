0bfce283f9ae979373c9045a223b10c0b72db170
Mauri2591 <mauriciorgonzalez25@gmail.com>
2026-09-14 14:08:07 -0300

    Se agrega Deteccion IA tools

--- config.py
+++ config.py
@@ -79,6 +79,6 @@ OSINT_JOB_TIMEOUTS = {
     'username_enumeration': 2700,               # 45 min
     'phishing_domain_detection': 1200,          # 20 min
     'web_technology_detection': 7200,          # 2 horas
-    'deteccion_ia_tools': 2700,                 # 45 minutos
+    'deteccion_ia_tools': 7200,                 # 2 horas
     'default': 3600                             # Fallback 1 hora
 }
